c1d96e872417585dc7a18c1a4a1adaa8b07397dd
Ralph Schaer <ralphschaer@gmail.com>
2020-08-01 19:15:30 +0200

    Upgrade libraries

---
+++ check_dependencies.bat
@@ -0,0 +1,55 @@
+cd D:\ws\blog2020\babelfishchat\client
+call ncu
+cd D:\ws\blog2020\babelfishchat\server
+call checkdep
+
+cd D:\ws\blog2020\capacitor-push\client
+call ncu
+cd D:\ws\blog2020\capacitor-push\server
+call checkdep
+
+cd D:\ws\blog2020\chat\client
+call ncu
+cd D:\ws\blog2020\chat\server
+call checkdep
+
+cd D:\ws\blog2020\cryptochat\client
+call ncu
+cd D:\ws\blog2020\cryptochat\server
+call checkdep
+
+cd D:\ws\blog2020\geophotos\app
+call ncu
+cd D:\ws\blog2020\geophotos\extract
+call checkdep
+
+cd D:\ws\blog2020\hashupgrade\argon2
+call checkdep
+cd D:\ws\blog2020\hashupgrade\jooq
+call checkdep
+cd D:\ws\blog2020\hashupgrade\md5
+call checkdep
+
+cd D:\ws\blog2020\hCaptcha\basic
+call checkdep
+cd D:\ws\blog2020\hCaptcha\ionic
+call ncu
+
+cd D:\ws\blog2020\jsengine
+call checkdep
+
+cd D:\ws\blog2020\locationvideo
+call ncu
+
+cd D:\ws\blog2020\nebular-start
+call ncu
+
+cd D:\ws\blog2020\protobuf-js2\client
+call ncu
+cd D:\ws\blog2020\protobuf-js2\server
+call checkdep
+
+cd D:\ws\blog2020\rxjs-websocket\client
+call ncu
+cd D:\ws\blog2020\rxjs-websocket\server
+call checkdep
